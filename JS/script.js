fb9cf7b503a3800c371e85b1010ef72b7bf649f2
Yannis <yannis.samba@gmail.com>
2019-06-07 16:15:37 +0200

    add pdf

--- JS/script.js
+++ JS/script.js
@@ -10,11 +10,25 @@ const bomb ={
   time:{
     min:0,
     sec:0,
-    ms:0,
+    cs:0,
   }
 
 }
-
-function startBomb (){
-  
+function setTimer(time){
+  bomb.time.min = time[0]
+  bomb.time.sec = time[1]
+  bomb.time.cs = time[2]
 }
+function changeTimer(cstime){
+  let seconds = (cstime / 1000) % 60 ;
+  let minutes = ((cstime / (1000*60)) % 60);
+  let centiseconds = cstime - (seconds+minutes) 
+  return([minutes,seconds,centiseconds])
+}
+function startBomb (){
+  for (let i =0; i <(bomb.time.min*60000+bomb.time.sec*1000+bomb.time.cs*10);i++){
+    setTimeout(changeTimer(i),i)
+  }
+}
+
+console.log(changeTimer(1500))
