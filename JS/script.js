d658acf487ba0ead3e1aa1ed5bff82d2b98a77e5
Yannis <yannis.samba@gmail.com>
2019-06-07 14:59:48 +0200

    start projet

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+const bomb ={
+  started:false,
+  paused:false,
+  wires:{
+    red:[1,"uncut"],
+    green:[1,"uncut"],
+    blue:[1,"uncut"]
+  },
+
+  time:{
+    min:0,
+    sec:0,
+    ms:0,
+  }
+
+}
