33187c4493cadf4b857e8c517c9b41960398679e
Yannis <yannis.samba@gmail.com>
2019-06-09 11:26:27 +0200

    change starting parameters, and wires initialization

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 
 class Timer {
 
-  constructor(time,timeInput) {
+  constructor(time, timeInput) {
     this.timediv = time
     this.timeinput = timeInput
     this.isPaused = true;
     this.isReseted = true;
     console.log('Created timer');
-    
+
   }
   loop() {
     const now = Date.now();
@@ -22,12 +22,12 @@ class Timer {
       return
     }
 
-      // converts the given time in ms to /minutes/secondes/centiseconds/  //
-      const now2 = new Date(this.remaining);
+    // converts the given time in ms to /minutes/secondes/centiseconds/  //
+    const now2 = new Date(this.remaining);
 
-      const minuteString = now2.getMinutes().toString().padStart(2, '0');
-      const secondString = now2.getSeconds().toString().padStart(2, '0')
-      const centisecString = Math.floor(now2.getMilliseconds() / 10).toString().padStart(2, '0')
+    const minuteString = now2.getMinutes().toString().padStart(2, '0');
+    const secondString = now2.getSeconds().toString().padStart(2, '0')
+    const centisecString = Math.floor(now2.getMilliseconds() / 10).toString().padStart(2, '0')
 
     this.timediv.innerHTML = `${minuteString}:${secondString}:${centisecString}`
     if (this.remaining <= 0) {
@@ -44,7 +44,7 @@ class Timer {
     if (this.isPaused || this.isReseted) {
       // in case of reset, time remaining 
       if (this.isReseted) {
-        this.remaining = this.timeinput.value*1000
+        this.remaining = this.timeinput.value * 1000
       }
       this.lastTick = Date.now();
       this.isPaused = false;
@@ -68,42 +68,42 @@ class Timer {
   reset() {
     this.isReseted = true
     this.timediv.innerHTML = "00:00:00"
-    
+
   }
 }
 class Wires {
-  constructor(wire_elements,wire_count){
+  constructor(wire_elements, wire_count) {
     this.allWires = wire_elements
     this.newWireCount = wire_count.value
 
   }
   // update will add/removes wires 
-  update(){
+  update() {
     // will make as many elements as there are wires visible
     console.log(this.allWires[3])
-    for(let i=0; i< this.newWireCount;i++){
+    for (let i = 0; i < this.newWireCount; i++) {
       this.allWires[i].removeAttribute("style")
     }
     // if number of wires selected inferior to the max, non selected wires becomes invisible
-    if(this.newWireCount > 0){
-      for(let i =0; i< 8-parseInt(this.newWireCount);i++){
-        this.allWires[7-i].style="display:none"
+    if (this.newWireCount > 0) {
+      for (let i = 0; i < 8 - parseInt(this.newWireCount); i++) {
+        this.allWires[7 - i].style = "display:none"
       }
     }
   }
-  cutWire(){
+  cutWire() {
     console.log("were in")
-    for (let i=0;i<this.newWireCount;i++){
+    for (let i = 0; i < this.newWireCount; i++) {
       console.log("hey")
       console.log(this.allWires[i].src)
-      this.allWires[i].addEventListener("click",()=>{
+      this.allWires[i].addEventListener("click", () => {
         this.allWires[i].src = "Assets/redcutwire.png"
         console.log("cut")
       })
     }
   }
-  wireSetting(){
-    
+  wireSetting() {
+
   }
 }
 
@@ -117,19 +117,23 @@ class UserI {
     let minus = document.getElementsByClassName("minus")
     let timeDiv = document.getElementById("timer")
     let timeInput = document.getElementById("time-input")
-    
+
     // Timer needs the elements of the seconds input, and the timer div.
-    let timer = new Timer(timeDiv,timeInput)
+    let timer = new Timer(timeDiv, timeInput)
 
     start.addEventListener("click", () => {
-      timer.start()
       // the wires input element has to be redefined each time start is pushed
-      let wires = document.getElementsByClassName("wires")
-      let wireCount = document.getElementById("wire-count")
-      let wires_ = new Wires(wires,wireCount)
-      wires_.update()
-      wires_.cutWire()
-      
+      console.log(timer.isReseted)
+      if(timer.isReseted)
+      {
+        let wires = document.getElementsByClassName("wires")
+        let wireCount = document.getElementById("wire-count")
+        let wires_ = new Wires(wires, wireCount)
+        wires_.update()
+        wires_.cutWire()
+      }
+      timer.start()
+
     })
     stop.addEventListener("click", () => {
       timer.pause()
@@ -139,11 +143,11 @@ class UserI {
       console.log("stopped this shit")
     })
     // })plus.addEventListener("click", ()=>{
-      
-      // })minus.addEventListener("click", ()=>{
-        
-      }
-      
+
+    // })minus.addEventListener("click", ()=>{
+
+  }
+
 }
 
 
